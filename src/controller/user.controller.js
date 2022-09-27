82a99569ab94536d862227c19ef3d02e8e037dee
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-09-27 13:33:29 +0530

    changes

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -5,40 +5,40 @@ const User = require("../model/user.model");
 router.post("/userdetailss", async(req, res) => {
     try{
         const userdetailss = await User.create(req.body);
-        return res.status(201).send(userdetails);
+        return res.status(201).send(userdetailss);
     }catch(err){
         return res.status(500).send({message: err.message}) 
     }
 })
 
-router.get("/userdetails", async(req, res) => {
+router.get("/userdetailss", async(req, res) => {
     try{
         const userdetails = await User.find().lean().exec();
-        return res.status(201).send(userdetails);
+        return res.status(201).send(userdetailss);
     }catch(err){
         return res.status(500).send({message: err.message}) 
     }
 })
 
-router.patch("/userdetails/:id", async(req, res)=>{
+router.patch("/userdetailss/:id", async(req, res)=>{
     try{
-        const userdetails = await User.findByIdAndUpdate(
+        const userdetailss = await User.findByIdAndUpdate(
             req.params.id,
             req.body,{
                 new: true
             })
-        return res.status(201).send(userdetails);
+        return res.status(201).send(userdetailss);
     }catch(err){
         return res.status(500).send({message: err.message})
     }
 })
 
-router.delete("/userdetails/:id", async(req, res)=>{
+router.delete("/userdetailss/:id", async(req, res)=>{
     try{
-        const userdetails = await User.findByIdAndDelete(
+        const userdetailss = await User.findByIdAndDelete(
             req.params.id
         )
-        return res.status(201).send(userdetails)
+        return res.status(201).send(userdetailss)
     }catch(err){
         return res.status(500).send({message: err.message})
     }
