f6cad0ab96e6b8355643dd5418e579411b7467fc
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-09-27 13:24:36 +0530

    user controller route changes

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -2,43 +2,43 @@ const express = require("express");
 const router = express.Router();
 const User = require("../model/user.model");
 
-router.post("/user", async(req, res) => {
+router.post("/userdetails", async(req, res) => {
     try{
         const food = await User.create(req.body);
-        return res.status(201).send(user);
+        return res.status(201).send(userdetails);
     }catch(err){
         return res.status(500).send({message: err.message}) 
     }
 })
 
-router.get("/user", async(req, res) => {
+router.get("/userdetails", async(req, res) => {
     try{
-        const user = await User.find().lean().exec();
-        return res.status(201).send(user);
+        const userdetails = await User.find().lean().exec();
+        return res.status(201).send(userdetails);
     }catch(err){
         return res.status(500).send({message: err.message}) 
     }
 })
 
-router.patch("/user/:id", async(req, res)=>{
+router.patch("/userdetails/:id", async(req, res)=>{
     try{
-        const user = await User.findByIdAndUpdate(
+        const userdetails = await User.findByIdAndUpdate(
             req.params.id,
             req.body,{
                 new: true
             })
-        return res.status(201).send(user);
+        return res.status(201).send(userdetails);
     }catch(err){
         return res.status(500).send({message: err.message})
     }
 })
 
-router.delete("/user/:id", async(req, res)=>{
+router.delete("/userdetails/:id", async(req, res)=>{
     try{
-        const user = await User.findByIdAndDelete(
+        const userdetails = await User.findByIdAndDelete(
             req.params.id
         )
-        return res.status(201).send(user)
+        return res.status(201).send(userdetails)
     }catch(err){
         return res.status(500).send({message: err.message})
     }
